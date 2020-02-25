e041ca588e36175a0a42caa3065c874fc8e25f7b
Vrafdom55 <61251939+Vrafdom55@users.noreply.github.com>
2020-02-25 10:41:45 -0500

    Add files via upload
    this is a test python script

---
+++ testing.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+#import scipy 
+import matplotlib.pyplot as plt 
+
+
+plt.plot([1,2,3,4])
+plt.ylabel('y label')
+plt.xlabel('x label')
+plt.show()
+
